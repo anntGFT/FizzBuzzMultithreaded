e54aef2f1b31ced9d228911f7bdea4bcd5b88c67
annt <annt@gft.com>
2022-11-25 10:52:08 +0000

    I coudn't do the tests, I only upload this for references pruposes

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,7 +1,61 @@
 package org.example;
 
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.function.IntConsumer;
+
+public class FizzBuzz {
+    private int n, counter = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
     }
+
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while(counter <= n){
+            if(counter % 3 == 0 && counter % 5 != 0){
+                printFizz.run();
+                counter++;
+                this.notifyAll();
+            }
+            else this.wait();
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while(counter <= n){
+            if(counter % 3 != 0 && counter % 5 == 0) {
+                printBuzz.run();
+                counter++;
+                this.notifyAll();
+            }
+            else this.wait();
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while(counter <= n){
+            if(counter % 3 == 0 && counter % 5 == 0) {
+                printFizzBuzz.run();
+                counter++;
+                this.notifyAll();
+            }
+            else this.wait();
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while(counter <= n){
+            if(counter % 3 != 0 && counter % 5 != 0) {
+                printNumber.accept(counter);
+                counter++;
+                this.notifyAll();
+            }
+            else this.wait();
+        }
+
+    }
+
 }
+
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,40 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class SolutionTest {
+
+    static FizzBuzz fizzBuzz;
+    static int num;
+    int result;
+
+    @BeforeAll
+    static void setup(){
+        fizzBuzz = new FizzBuzz(num);
+    }
+
+    @Test
+    public void given2returnListOfStrings() throws InterruptedException {
+        num = 2;
+
+
+    }
+
+    @Test
+    public void given3returnListOfStrings() {
+        num = 3;
+
+
+    }
+    @Test
+    public void given15returnListOfStrings(){
+        num = 15;
+
+
+
+    }
 }
